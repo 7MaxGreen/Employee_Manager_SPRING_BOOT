39cdb09770511fa785f73da1745a7b42d40b469e
Max <maximnicola7@gmail.com>
2022-05-23 19:57:08 +0300

    Code refactoring

--- src/main/java/edu/step/employeeManager/controller/CompanyController.java
+++ src/main/java/edu/step/employeeManager/controller/CompanyController.java
@@ -1,6 +1,7 @@
 package edu.step.employeeManager.controller;
 
 import edu.step.employeeManager.dto.CompanyDTO;
+import edu.step.employeeManager.exceptions.CriteriaNotMatchingException;
 import edu.step.employeeManager.exceptions.EntityNotFoundException;
 import edu.step.employeeManager.model.Company;
 import edu.step.employeeManager.repository.CompanyRepository;
@@ -24,7 +25,7 @@ public class CompanyController {
         return dtoService.getAll();
     }
     @PostMapping("/create")
-    public void create(@RequestBody CompanyDTO companyDTO){
+    public void create(@RequestBody CompanyDTO companyDTO) throws CriteriaNotMatchingException, EntityNotFoundException {
         dtoService.create(companyDTO);
     }
     @PutMapping("/update")
@@ -32,12 +33,12 @@ public class CompanyController {
         try{
             dtoService.update(companyDTO);
             return ResponseEntity.ok().build();
-        } catch (EntityNotFoundException ex) {
+        } catch (EntityNotFoundException | CriteriaNotMatchingException ex) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
         }
     }
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity delete(@PathVariable("id") Integer id) {
+    public ResponseEntity delete(@PathVariable("id") Integer id) throws EntityNotFoundException {
         boolean result = dtoService.delete(id);
         if(result){
             return ResponseEntity.ok().build();
